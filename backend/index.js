d36f56a5b41e9c8e299ece18affea242f4d48382
abderrahimrekkas <rekkasabderrahim8@gmail.com>
2025-05-19 13:42:33 +0200

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,38 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import { connectDB } from "./config/connectDB.js";
-import authRouter from "./routes/authRouter.js";
-import path from "path";
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import { connectDB } from "./config/connectDB.js";
+// import authRouter from "./routes/authRouter.js";
+// import path from "path";
 
-dotenv.config();
+// dotenv.config();
 
-const server=express();
-const port=process.env.PORT || 8083;
-const __dirname=path.resolve();
+// const server=express();
+// const port=process.env.PORT || 8083;
+// const __dirname=path.resolve();
 
-server.use(cors({origin: "http://localhost:5174", credentials: true}));
+// server.use(cors({origin: "http://localhost:5174", credentials: true}));
 
-server.use(express.json());
-server.use(cookieParser());
+// server.use(express.json());
+// server.use(cookieParser());
 
-// server.get("/", (req, res)=> {
-//     res.send("Welcome to the server");
+// // server.get("/", (req, res)=> {
+// //     res.send("Welcome to the server");
+// // });
+
+// server.use("/api/auth", authRouter);
+
+// if(process.env.NODE_ENV==="production"){
+//     server.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+//     server.get("*", (req, res)=> {
+//         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//     });
+// }
+
+// server.listen(port, ()=> {
+//     connectDB();
+//     console.log("Server running at port", port);
 // });
-
-server.use("/api/auth", authRouter);
-
-if(process.env.NODE_ENV==="production"){
-    server.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-    server.get("*", (req, res)=> {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    });
-}
-
-server.listen(port, ()=> {
-    connectDB();
-    console.log("Server running at port", port);
-});
+console.log("Server running at port", port);
